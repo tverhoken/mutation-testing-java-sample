a14627a4cbf207868f7fdfbb5ac9af53b5a9f4d0
tverhoken <tverhoken@norsys.fr>
2023-08-30 16:39:31 +0200

    Don't forget about parametrized tests

--- src/test/java/fr/norsys/tverhoken/mutationtesting/yolo/CalculatorTest.java
+++ src/test/java/fr/norsys/tverhoken/mutationtesting/yolo/CalculatorTest.java
@@ -1,8 +1,9 @@
 package fr.norsys.tverhoken.mutationtesting.yolo;
 
-import fr.norsys.tverhoken.mutationtesting.yolo.Calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,6 +13,13 @@ public class CalculatorTest {
     @DisplayName("Should perform an addition with provided args.")
     public void addition0and0() {
         assertThat(new Calculator().addition(0, 0)).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,0,0", "1,1,2", "2,2,4"})
+    @DisplayName("Should perform an addition and return expected result when provided args are.")
+    void additionTest(int arg1, int arg2, int expectedResults) throws Exception {
+        assertThat(new Calculator().addition(arg1, arg2)).isEqualTo(expectedResults);
     }
 
 }
